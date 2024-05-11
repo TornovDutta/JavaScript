0612fd99077778d3023e7babaff8b82c62d4e2a6
TornovDutta <162907041+TornovDutta@users.noreply.github.com>
2024-05-11 13:07:27 +0530

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,37 @@
+console.log("hello world");
+fullname="tony ";
+console.log(fullname);
+let name="rohit";
+console.log(name);
+var fullname="rohit majumder";
+console.log(fullname);
+// ES6
+// ES6 is new update of javaScript
+let age=26;
+console.log(age);
+let a;
+console.log(a);//underfined
+// let age=25;
+// can'nt re decleared
+
+age=4;
+console.log(age);
+
+const num=24;
+// num=4;
+// can't redeclear or update
+console.log(num);
+
+
+// data type
+typeof num;
+x=null;
+
+
+
+// object 
+const student={
+    a:3,
+    b:"hello bachooo"
+
+}
